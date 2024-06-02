15530948dbae9d222efcbb21eb70b032ec64b306
Jackson Kaplan <JwyattK@gmail.com>
2024-06-02 15:15:07 -0600

    basic word fetcher

--- src/fetch.rs
+++ src/fetch.rs
@@ -1,1 +1,33 @@
-extern crate reqwest;
+use std::io::Write;
+
+fn create_words(path: std::path::PathBuf) {
+    if path.exists() {
+        return;
+    }
+
+    let url = "https://github.com/dwyl/english-words/raw/master/words_alpha.zip";
+
+    let response = match rq::blocking::get(url) {
+        Ok(res) => res,
+        Err(_) => unimplemented!(),
+    };
+
+    let body = match response.text() {
+        Ok(b) => b,
+        Err(_) => unimplemented!(),
+    };
+
+    std::fs::create_dir("./data").unwrap();
+
+    std::fs::create_dir_all(path.parent().unwrap()).unwrap();
+
+    match std::fs::File::create_new(path) {
+        Ok(mut f) => f.write_all(body.as_bytes()).unwrap(),
+        Err(_) => (),
+    }
+}
+
+pub fn get_words() {
+    let file: std::path::PathBuf = ["data", "words.txt"].iter().collect();
+    create_words(file);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,7 @@
 mod fetch;
 mod parse;
-extern crate std;
 
-fn main() -> std::io::Result<()> {
-    use std::fs;
-    for entry in fs::read_dir(".")? {
-        let entry = entry?;
-        let path = entry.path();
-
-        match path.to_str() {
-            Some(p) => println!("{p}"),
-            None => (),
-        }
-    }
-    println!("Hello world!");
-
-    Ok(())
+fn main() {
+    println!("Hello, world!");
+    fetch::get_words()
 }
