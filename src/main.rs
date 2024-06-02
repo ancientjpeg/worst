d6c96f9fe103927d12bc3b301a603b392713de36
Jackson Kaplan <JwyattK@gmail.com>
2024-06-02 14:12:53 -0600

    begin work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod fetch;
+mod parse;
+extern crate std;
+
+fn main() -> std::io::Result<()> {
+    use std::fs;
+    for entry in fs::read_dir(".")? {
+        let entry = entry?;
+        let path = entry.path();
+
+        match path.to_str() {
+            Some(p) => println!("{p}"),
+            None => (),
+        }
+    }
+    println!("Hello world!");
+
+    Ok(())
 }
