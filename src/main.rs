65a223d729b33932d76c369e7425e87be65fceaa
Jackson Kaplan <JwyattK@gmail.com>
2024-06-21 09:50:06 -0700

    add prefetch file - change retval to occurrences !

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,31 @@ mod fetch;
 mod gutenberg;
 mod utils;
 
+use rand::prelude::*;
+
 fn main() {
-    // analyzer::analyze();
-    let word = "technical";
+    let word_rates = match analyzer::analyze() {
+        Some(val) => val,
+        None => return,
+    };
+
+    let mut filtered_words = word_rates.iter().filter(|(_, v)| {
+        let occ = **v as f32 / word_rates.len() as f32;
+        return occ < 0.001 && occ > 0.0005;
+    });
+
+    let filtered_count = filtered_words.clone().count();
+
+    let mut r = thread_rng();
+    let i: usize = r.gen_range(0..filtered_count);
+
+    for (w, v) in word_rates.iter() {
+        println!("{w}: {v}");
+    }
+
+    let word = filtered_words.nth(i).unwrap().0;
     let result = dictionary::get_rq(word);
+
     match result {
         Ok(r) => println!("Definition of {}:\n{}", word, r.definition),
         Err(e) => println!("Definition error: {}", e),
