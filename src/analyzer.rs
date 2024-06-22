02f7a014969f1f7c07c4fb4dae57a65ea784deb7
Jackson Kaplan <JwyattK@gmail.com>
2024-06-21 21:27:14 -0700

    proper results serde

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,89 +1,68 @@
-use std::fs;
-use std::io::BufRead;
-use std::io::BufReader;
-use std::io::BufWriter;
-use std::io::Write;
+use std::io;
 
-use regex::Regex;
+use serialization::ToFile;
 
 use crate::fetch;
 use crate::gutenberg;
 use crate::utils;
 
+mod serialization;
+
+fn word_counts_to_prevalence(
+    counts: &fetch::WordCountMap,
+    total_words: usize,
+) -> fetch::WordPrevalenceMap {
+    let total = total_words as f32;
+    let mut ret = fetch::WordPrevalenceMap::new();
+
+    for (k, v) in counts.iter() {
+        let prevalence = *v as f32 / total;
+        ret.insert(k.to_string(), prevalence);
+    }
+
+    ret
+}
+
 #[allow(dead_code)] // TODO remove
-pub fn analyze() -> Result<fetch::WordMap, Box<dyn std::error::Error>> {
+pub fn analyze() -> io::Result<fetch::WordPrevalenceMap> {
     let ofile = utils::get_app_tempdir_child("output.txt");
 
     // prefetch if ofile exists
     if ofile.exists() {
-        let mut ret = fetch::WordMap::new();
-
-        let file = fs::File::open(&ofile)?;
-        let reader = BufReader::new(file);
-        let re = Regex::new(r"word: (\w+)\s+prevalence: (.*)%").unwrap();
-
-        let lines: Vec<_> = reader.lines().collect();
-
-        let wc = lines.len() as f32;
-
-        for l in lines {
-            let line = l.unwrap();
-            let caps = re.captures(&line).unwrap();
-            assert_eq!((&caps).len(), 3usize);
-
-            let prevalence: f32 = caps[2].parse().unwrap();
-            let count = (wc * prevalence).round() as usize;
-            if count == 0 {
-                continue;
-            }
-            ret.insert(caps[1].to_string(), count);
-        }
-        return Ok(ret);
+        return serialization::FromFile::from_file(&ofile);
     }
 
-    let mut word_map = fetch::get_words()?;
+    let mut count_map = fetch::get_words()?;
     let word_data = gutenberg::get_gutenberg_data()?;
 
     let word_data_split = word_data.split_whitespace();
 
     println!("Begin wordcount.");
-    let wordcount: f32 = word_data_split.clone().count() as f32;
+    let wordcount = word_data_split.clone().count();
 
     println!("Wordcount: {}", wordcount);
 
     println!("Begin analysis.");
     for (i, word) in word_data_split.enumerate() {
-        if word_map.contains_key(word) {
-            let val = word_map.get_mut(word).unwrap();
+        if count_map.contains_key(word) {
+            let val = count_map.get_mut(word).unwrap();
             *val += 1usize;
             // } else {
             //     println!("Missed a word! {:20}", word);
         }
 
         if i % 10_000 == 0 && i > 0 {
-            let complete_pct = i as f32 / wordcount;
+            let complete_pct = i as f32 / wordcount as f32;
             crate::utils::print_status_bar(complete_pct)
         }
     }
-
     println!("");
 
-    let mut values: Vec<(&String, &usize)> = word_map.iter().map(|(k, v)| (k, v)).collect();
-    values.sort_by(|(a, _), (b, _)| a.len().cmp(&b.len()));
-    values.sort_by(|(_, a), (_, b)| b.cmp(a));
+    count_map.retain(|_, &mut v| v != 0);
 
-    let handle = fs::File::create(ofile).unwrap();
+    let prevalence_map = word_counts_to_prevalence(&count_map, wordcount);
 
-    let mut writer = BufWriter::new(handle);
+    prevalence_map.to_file(&ofile)?;
 
-    for (k, v) in values.iter() {
-        if **v == 0 {
-            continue;
-        };
-        let prevalence = **v as f32 / wordcount * 100.;
-        let line = format!("word: {:<25} prevalence: {:.6}%\n", k, prevalence);
-        writer.write(line.as_bytes()).unwrap();
-    }
-
-    Ok(word_map)
+    Ok(prevalence_map)
 }
--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,40 @@ use rand::prelude::*;
 fn main() {
     let word_rates = match analyzer::analyze() {
         Ok(val) => val,
-        Err(_) => return,
+        Err(e) => {
+            println!("Analysis failed due to error: {}", e);
+            return;
+        }
     };
 
-    let mut filtered_words = word_rates.iter().filter(|(_, v)| {
+    let filtered_words = word_rates.iter().filter(|(k, v)| {
         let occ = **v as f32 / word_rates.len() as f32;
-        return occ < 0.001 && occ > 0.0005;
+        if k.ends_with("ed") {
+            return false;
+        }
+        if k.ends_with("ly") {
+            return false;
+        }
+        if k.ends_with("s") {
+            return false;
+        }
+        return (0.0..0.000001).contains(&occ) && k.len() > 8;
     });
 
     let filtered_count = filtered_words.clone().count();
 
-    let mut r = thread_rng();
-    let i: usize = r.gen_range(0..filtered_count);
+    println!("Match count: {filtered_count}");
+    for _ in 0..filtered_count {
+        let mut r = thread_rng();
+        let i: usize = r.gen_range(0..filtered_count);
 
-    for (w, v) in word_rates.iter() {
-        println!("{w}: {v}");
-    }
+        let (word, prev) = filtered_words.clone().nth(i).unwrap();
+        println!("Chose word {word} with prevalence {:.6}%", *prev * 100.);
+        let result = dictionary::get_rq(word);
 
-    let word = filtered_words.nth(i).unwrap().0;
-    let result = dictionary::get_rq(word);
-
-    match result {
-        Ok(r) => println!("Definition of {}:\n{}", word, r.definition),
-        Err(e) => println!("Definition error: {}", e),
+        match result {
+            Ok(r) => println!("Definition of {}:\n{}\n", word, r.definition),
+            Err(e) => println!("Definition error: {}", e),
+        }
     }
 }
